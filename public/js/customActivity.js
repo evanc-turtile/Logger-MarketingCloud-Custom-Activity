dddee647d25e3d5ef7685b1ae193677e2502844d
evanc-turtile <evanchen.cs@gmail.com>
2021-12-07 09:36:02 -0500

    attempting to fix saving error

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -21,9 +21,6 @@ define(['postmonger'], function(Postmonger) {
 		payload['arguments'] = payload['arguments'] || {};
 		payload['arguments'].execute = payload['arguments'].execute || {};
 
-		payload['arguments'].execute.inArguments = [{
-		}];
-
 		payload['metaData'] = payload['metaData'] || {};
 		payload['metaData'].isConfigured = true;
 
