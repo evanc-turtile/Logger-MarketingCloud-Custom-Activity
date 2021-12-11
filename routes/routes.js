5cbd4be0f1c199488a85dd15a11a1fba52c3be17
evanc-turtile <evanchen.cs@gmail.com>
2021-12-11 00:21:18 -0500

    attempting to fix endpoint calls

--- routes/routes.js
+++ routes/routes.js
@@ -29,7 +29,7 @@ routes.post('/execute', function(req, res, next) {
 });
 
 routes.post('/save', function(req, res, next) {
-	validateConfigurations(req);
+	validateConfigurations(req.body);
 	//res.status(200).json({});
 	res.send(200, 'Save')
 });
